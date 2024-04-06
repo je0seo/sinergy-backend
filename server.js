361bc8958d0d165b41e3fa998663590b49244c9b
Sini0206 <137382250+Sini0206@users.noreply.github.com>
2024-04-06 18:13:37 +0900

    feat(server): 별칭으로 검색했을 때도 poi_point에 존재하는 해당 노드의 정보를 요청하도록 쿼리 추가

--- server.js
+++ server.js
@@ -261,7 +261,8 @@ async function getBuildingInfoAsync(req) {
                          FROM bd_info as b
                          INNER JOIN poi_point as p
                          ON b.bg_name = p.bg_name
-                         WHERE p.bg_name LIKE '%${req.keyword}%'`;
+                         WHERE p.bg_name LIKE '%${req.keyword}%'
+                         OR p.nickname LIKE '%${req.keyword}%'`;
     const queryResult = await client.query(queryString);
     return queryResult;
 }
