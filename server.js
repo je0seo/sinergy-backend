9b7c1820902da2b1b70a889098ac30279286d252
je0seo <jeyoungseo@gmail.com>
2024-04-09 21:18:05 +0900

    도서관, 자판기 처리 추가

--- server.js
+++ server.js
@@ -283,6 +283,12 @@ async function ShowReqAsync(requestDatatype) {
         else if (requestDatatype.Req === 'Sbicycle') { //따릉이대여소
             Id4ShowQuery += ` WHERE n.conv_cate = 16`;
         }
+        else if (requestDatatype.Req === 'library') { //도서관
+            Id4ShowQuery += ` WHERE n.conv_cate = 17`;
+        }
+        else if (requestDatatype.Req === 'vendingMachine') { //자판기
+            Id4ShowQuery += ` WHERE n.conv_cate = 18`;
+        }
         else if (requestDatatype.Req === 'facilities') {
             Id4ShowQuery += ` WHERE n.node_att = 8`;
         }
